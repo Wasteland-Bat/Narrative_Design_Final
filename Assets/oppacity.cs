1aa85f27aaf1622610321de289c816fe5dbe15d5
Wasteland-Bat <132709552+Wasteland-Bat@users.noreply.github.com>
2023-11-29 16:17:40 -0500

    first Commit

---
+++ Assets/oppacity.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oppacity : MonoBehaviour
+{
+    public GameObject sprite;
+ SpriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
+}
